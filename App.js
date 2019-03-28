5870c7c8a6edf93adbd2ef9adcd15131ffd04da9
FarighMAkmalullah <48850497+FarighMAkmalullah@users.noreply.github.com>
2019-03-28 20:36:43 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import Register from './Layout/Register/Index';
+import Login from './Layout/LogIn/Index';
+
+class App extends React.Component{
+  render(){
+    return(
+      <Login/>
+    )
+  }
+}
+export default App;
